0ecdb28eb69578fe242c3273018dc1eaf20f743e
Pouya <pouya.mozafar@gmail.com>
2020-05-24 12:43:34 +0430

    refactor(structure): refactor to new structure

--- src/main.js
+++ src/main.js
@@ -1,13 +1,3 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import startUI from "./GUI/main";
 
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+startUI();
