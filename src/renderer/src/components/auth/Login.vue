fd61b29747e5c7c4b410d220537a203e79ed8ea8
Pouya <pouya.mozafar@gmail.com>
2020-05-24 21:32:19 +0430

    feat(renderer): add scss and vues

---
+++ src/renderer/src/components/auth/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <VContainer class="login">
+    <VRow justify="center" align="center" class="login__row">
+      <VCol cols="6">
+        <VRow justify="center">
+          ورود
+        </VRow>
+        <VRow>
+          <VCol cols="12">
+            <VForm>
+              <VTextField label="نام کاربری" v-model="username" />
+              <VTextField label="گذرواژه" v-model="password" />
+
+              <VBtn color="primary">ورود</VBtn>
+            </VForm>
+          </VCol>
+        </VRow>
+      </VCol>
+    </VRow>
+  </VContainer>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.login {
+  height: 100vh;
+  &__row {
+    height: 100%;
+  }
+}
+</style>
---
+++ src/renderer/src/router/routes.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    path: "/",
+    name: "login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "auth" */ "@/components/auth/Login.vue")
+  }
+];
