8732d5208133d47e1e44b5731f2e2c07e8190301
Pouya <pouya.mozafar@gmail.com>
2020-05-25 17:36:39 +0430

    style(renderer): add PascalCase component rule

--- src/renderer/.eslintrc.js
+++ src/renderer/.eslintrc.js
@@ -3,14 +3,22 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/recommended", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
     parser: "babel-eslint"
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/component-name-in-template-casing": [
+      "error",
+      "PascalCase",
+      {
+        registeredComponentsOnly: false
+      }
+    ]
   },
+  plugins: ["vue"],
   overrides: [
     {
       files: [
